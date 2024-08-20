074637f95915a9fc7ac7a9e8dc75d4e1471bd9f3
Isa Albayrak <isaalbayrak66@icloud.com>
2024-08-21 00:03:36 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        bebas: ['Bebas Neue', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
